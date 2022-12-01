d42bb9ab3529c36cd5f96d4440caeff30c2defed
jaeyi <plotojjy@gmail.com>
2022-12-01 12:53:04 +0900

    J2 user 기능 구현중

--- src/main/java/com/kh/devs_server/controller/UserController.java
+++ src/main/java/com/kh/devs_server/controller/UserController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,7 +56,7 @@ public class UserController {
         }
     }
 
-    @PutMapping("/update")
+//    @PutMapping("/update")
 
 
 }
